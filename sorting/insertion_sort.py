a281f762c9adc83388bec6854f42e9665896004d
steevenmendozau <steeven.mendoza@uees.edu.ec>
2025-06-07 14:20:35 -0500

    Generacion de los datos de Prueba

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,11 @@
+def SelectionSortTest(array):
+    for i in range(len(array)):
+        min_idx = i
+        for j in range(i+1, len(array)):
+            if array[j] < array[min_idx]:
+                min_idx = j
+        array[i], array[min_idx] = array[min_idx], array[i]
+    return array
+
+def InsertionSort():
+    pass
