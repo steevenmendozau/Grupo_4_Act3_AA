c2ef9cb79e2cf74ad19c613471e98c2cc92bdf21
Alejandro Lascano <alejandrolascano126@gmail.com>
2025-06-08 15:54:40 -0500

    bubble sort benchmark v1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from sorting.selection_sort import cargar_gastos_csv, selection_sort
-from utils.time_benchmark import benchmark_selection_sort
+from utils.time_benchmark import benchmark_bubble_sort, benchmark_selection_sort
 
 
 # gastos = cargar_gastos_csv('data/gastos_50.csv')
@@ -11,3 +11,9 @@ benchmark_selection_sort('fecha')
 benchmark_selection_sort('descripcion')
 benchmark_selection_sort('monto')
 benchmark_selection_sort('mes')   
+
+
+benchmark_bubble_sort('fecha')
+benchmark_bubble_sort('descripcion')
+benchmark_bubble_sort('monto')
+benchmark_bubble_sort('mes') 
--- utils/time_benchmark.py
+++ utils/time_benchmark.py
@@ -1,4 +1,5 @@
 import timeit
+from sorting.bubble_sort import bubbleSort
 from sorting.selection_sort import selection_sort,cargar_gastos_csv
 import pandas as pd
 import os
@@ -25,6 +26,47 @@ def benchmark_selection_sort(clave):
 
         resultados.append({
     'algoritmo': 'Selection Sort',
+    'archivo': archivo,
+    'criterio': clave,
+    'minimo': round(minimo, 6),
+    'promedio': round(promedio, 6),
+    'maximo': round(maximo, 6)
+})
+
+    df = pd.DataFrame(resultados)
+    output_file = 'data/benchmark_resultados.csv'
+
+    if os.path.exists(output_file):
+        df.to_csv(output_file, mode='a', header=False, index=False)
+    else:
+        df.to_csv(output_file, mode='w', header=True, index=False)
+
+    return resultados
+
+
+
+def benchmark_bubble_sort(clave):
+    archivos = ['data/gastos_50.csv', 'data/gastos_100.csv', 'data/gastos_1000.csv']
+    resultados = []
+
+    for archivo in archivos:
+        try:
+            datos = cargar_gastos_csv(archivo)
+        except FileNotFoundError:
+            print(f"Archivo no encontrado: {archivo}")
+            continue
+
+        def wrapper():
+            copia = datos.copy()
+            bubbleSort(copia, clave)
+
+        tiempos = timeit.repeat(stmt=wrapper, repeat=5, number=1)
+        minimo = min(tiempos)
+        promedio = sum(tiempos)/len(tiempos)
+        maximo = max(tiempos)
+
+        resultados.append({
+    'algoritmo': 'Bubble Sort',
     'archivo': archivo,
     'criterio': clave,
     'minimo': round(minimo, 6),
