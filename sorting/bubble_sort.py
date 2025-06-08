91bfa1d342c3bd64e97466d58d43e631ef28213d
Alejandro Lascano <alejandrolascano126@gmail.com>
2025-06-08 15:46:16 -0500

    bubble sort benchmark v1

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,8 @@
+def bubbleSort(gastos, clave):
+    n = len(gastos)
+    for i in range(0, n - 1):
+        for j in range(0, n - 1 - i):
+            if(gastos[j+1][clave] < gastos[j][clave]):
+              gastos[j][clave], gastos[j+1][clave] = gastos[j+1][clave], gastos[j][clave]
+    return gastos
+
