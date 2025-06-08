78181dd5c6e164ba45531535d2b2e8804408b146
steevenmendozau <steeven.mendoza@uees.edu.ec>
2025-06-07 21:04:03 -0500

    Finalizacion Selection Sort

---
+++ main.py
@@ -0,0 +1,13 @@
+from sorting.selection_sort import cargar_gastos_csv, selection_sort
+from utils.time_benchmark import benchmark_selection_sort
+
+
+# gastos = cargar_gastos_csv('data/gastos_50.csv')
+# gastos_ordenados = selection_sort(gastos, 'monto')
+# for gasto in gastos_ordenados[:10]:  # mostrar solo los primeros 10
+#     print(gasto)
+
+benchmark_selection_sort('fecha')
+benchmark_selection_sort('descripcion')
+benchmark_selection_sort('monto')
+benchmark_selection_sort('mes')   
--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -1,8 +1,19 @@
-def SelectionSort(array):
-    for i in range(len(array)):
+import csv
+
+
+def cargar_gastos_csv(ruta_archivo):
+    with open(ruta_archivo, mode='r', encoding='utf-8') as archivo:
+        lector = csv.DictReader(archivo)
+        gastos = [dict(fila) for fila in lector]  # por si acaso aseguramos el tipo dict
+        for gasto in gastos:
+            gasto['monto'] = float(gasto['monto'])  # convertir a número
+        return gastos
+
+def selection_sort(gastos, clave):
+    for i in range(len(gastos)):
         min_idx = i
-        for j in range(i+1, len(array)):
-            if array[j] < array[min_idx]:
+        for j in range(i + 1, len(gastos)):
+            if gastos[j][clave] < gastos[min_idx][clave]:
                 min_idx = j
-        array[i], array[min_idx] = array[min_idx], array[i]
-    return array
+        gastos[i], gastos[min_idx] = gastos[min_idx], gastos[i]
+    return gastos  
