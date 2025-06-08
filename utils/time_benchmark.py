a679676ca464a8986789d61ce871def4ce9897a2
Alejandro Lascano <alejandrolascano126@gmail.com>
2025-06-08 15:53:21 -0500

    Revert "bubble sort benchmark v1"
    This reverts commit 91bfa1d342c3bd64e97466d58d43e631ef28213d.

--- utils/time_benchmark.py
+++ utils/time_benchmark.py
@@ -1,5 +1,4 @@
 import timeit
-from sorting.bubble_sort import bubbleSort
 from sorting.selection_sort import selection_sort,cargar_gastos_csv
 import pandas as pd
 import os
@@ -42,46 +41,3 @@ def benchmark_selection_sort(clave):
         df.to_csv(output_file, mode='w', header=True, index=False)
 
     return resultados
-
-
-
-def benchmark_bubble_sort(clave):
-    archivos = ['data/gastos_50.csv', 'data/gastos_100.csv', 'data/gastos_1000.csv']
-    resultados = []
-
-    for archivo in archivos:
-        try:
-            datos = cargar_gastos_csv(archivo)
-        except FileNotFoundError:
-            print(f"Archivo no encontrado: {archivo}")
-            continue
-
-        def wrapper():
-            copia = datos.copy()
-            bubbleSort(copia, clave)
-
-        tiempos = timeit.repeat(stmt=wrapper, repeat=5, number=1)
-        minimo = min(tiempos)
-        promedio = sum(tiempos)/len(tiempos)
-        maximo = max(tiempos)
-
-        resultados.append({
-    'algoritmo': 'Bubble Sort',
-    'archivo': archivo,
-    'criterio': clave,
-    'minimo': round(minimo, 6),
-    'promedio': round(promedio, 6),
-    'maximo': round(maximo, 6)
-})
-
-    df = pd.DataFrame(resultados)
-    output_file = 'data/benchmark_resultados.csv'
-
-    if os.path.exists(output_file):
-        df.to_csv(output_file, mode='a', header=False, index=False)
-    else:
-        df.to_csv(output_file, mode='w', header=True, index=False)
-
-    return resultados
-
-
